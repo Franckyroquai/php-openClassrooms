76602e4f7debbc8062b13d0a19e4822e7a2ea732
Franckyroquai <franckyroquai@gmail.com>
2024-05-12 13:25:04 +0200

    insertion login et condition identification pour affichage des recetttes

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
+<!-- inclusion des variables et fonctions -->
 <?php
 require_once(__DIR__ . '/variables.php');
 require_once(__DIR__ . '/functions.php');
 ?>
+
 <!DOCTYPE html>
 <html>
-
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,23 +14,27 @@ require_once(__DIR__ . '/functions.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
         <!-- inclusion de l'entête du site -->
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Site de recettes</h1>
-        <?php foreach (getRecipes($recipes) as $recipe) : ?>
-            <article>
-                <h3><?php echo $recipe['title']; ?></h3>
-                <div><?php echo $recipe['recipe']; ?></div>
-                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
-            </article>
-        <?php endforeach ?>
+
+        <!-- Formulaire de connexion -->
+        <?php require_once(__DIR__ . '/login.php'); ?>
+
+        <?php if(isset($loggedUser)) : ?>
+            <?php foreach (getRecipes($recipes) as $recipe) : ?>
+                <article>
+                    <h3><?php echo $recipe['title']; ?></h3>
+                    <div><?php echo $recipe['recipe']; ?></div>
+                    <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+                </article>
+            <?php endforeach ?>
+        <?php endif; ?>
     </div>
+
     <!-- inclusion du bas de page du site -->
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
-
-
 </html>
