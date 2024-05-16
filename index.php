42795b86a6d36c2b28b1977aa75447dd5af5bf71
Franckyroquai <franckyroquai@gmail.com>
2024-05-16 17:48:49 +0200

    ajout lien vers recipes_read

--- index.php
+++ index.php
@@ -14,9 +14,11 @@ require_once(__DIR__ . '/functions.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Page d'accueil</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+    >
 </head>
-
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
         <!-- inclusion de l'entête du site -->
@@ -26,19 +28,19 @@ require_once(__DIR__ . '/functions.php');
         <!-- Formulaire de connexion -->
         <?php require_once(__DIR__ . '/login.php'); ?>
 
-            <?php foreach (getRecipes($recipes) as $recipe) : ?>
-                <article>
-                    <h3><?php echo $recipe['title']; ?></h3>
-                    <div><?php echo $recipe['recipe']; ?></div>
-                    <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
-                    <?php if (isset($_SESSION['LOGGED_USER']) && $recipe['author'] === $_SESSION['LOGGED_USER']['email']) : ?>
-                        <ul class="list-group list-group-horizontal">
+        <?php foreach (getRecipes($recipes) as $recipe) : ?>
+            <article>
+                <h3><a href="recipes_read.php?id=<?php echo($recipe['recipe_id']); ?>"><?php echo($recipe['title']); ?></a></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+                <?php if (isset($_SESSION['LOGGED_USER']) && $recipe['author'] === $_SESSION['LOGGED_USER']['email']) : ?>
+                    <ul class="list-group list-group-horizontal">
                         <li class="list-group-item"><a class="link-warning" href="recipes_update.php?id=<?php echo($recipe['recipe_id']); ?>">Editer l'article</a></li>
                         <li class="list-group-item"><a class="link-danger" href="recipes_delete.php?id=<?php echo($recipe['recipe_id']); ?>">Supprimer l'article</a></li>
-                        </ul>
-                    <?php endif; ?>
-                </article>
-            <?php endforeach ?>
+                    </ul>
+                <?php endif; ?>
+            </article>
+        <?php endforeach ?>
     </div>
 
     <!-- inclusion du bas de page du site -->
